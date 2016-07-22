d68f53dadea05c96edb066dd5e56204bcc3509c9
Dusan Ristic <dusan.ristic@revelsystems.com>
2016-07-22 12:27:32 +1000

    remove --rm for build images

--- scripts/build.sh
+++ scripts/build.sh
@@ -5,5 +5,5 @@ set -e # Exit on errors
 for image in bamboo-server 
 do
     echo "=> Building $image ..."
-    docker build -rm -t dusan87/$image:latest $image
+    docker build -t dusan87/$image:latest $image
 done
